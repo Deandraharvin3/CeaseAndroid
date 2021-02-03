09c5cd464a965a3ce953df8adf25d5509e55a820
deandraharvin <deandraharvin3@gmail.com>
2021-02-02 22:02:28 -0500

    creating home page

---
+++ app/src/main/java/com/example/android/Login.java
@@ -0,0 +1,41 @@
+package com.example.android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.regex.Pattern;
+
+public class Login extends AppCompatActivity {
+
+    Button nextbtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        TextView tvSignup = (TextView) findViewById(R.id.tvSignup);
+        tvSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Login.this, MainActivity2.class);
+                startActivity(i);
+            }
+        });
+
+        nextbtn = findViewById(R.id.nextbtn);
+        nextbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Login.this, MainActivity2.class);
+                startActivity(i);
+            }
+        });
+    }
+}
