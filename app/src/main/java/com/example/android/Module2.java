5dd70acdcf475ba2ca29ca0ba79ab6d61d4d7c93
deandraharvin <deandraharvin3@gmail.com>
2021-02-10 12:41:56 -0500

    creating lesson and module files

--- app/src/main/java/com/example/android/Module2.java
+++ app/src/main/java/com/example/android/Module2.java
@@ -2,13 +2,26 @@ package com.example.android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Module2 extends AppCompatActivity {
+    ImageView mod2btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_module2);
+
+        mod2btn = findViewById(R.id.mod2btn);
+        mod2btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Module2.this, Lesson4.class);
+                startActivity(i);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/android/Module2Complete.java
@@ -0,0 +1,14 @@
+package com.example.android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Module2Complete extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_module2_complete);
+    }
+}
