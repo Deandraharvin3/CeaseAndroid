cb333ac50444fdb345b98dac144797d469ac5a4d
deandraharvin <deandraharvin3@gmail.com>
2021-02-11 13:52:46 -0500

    completing lession 4 view

--- app/src/main/java/com/example/android/Lesson4.java
+++ app/src/main/java/com/example/android/Lesson4.java
@@ -2,13 +2,30 @@ package com.example.android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Lesson4 extends AppCompatActivity {
+
+    ImageView btn4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lesson4);
+
+        btn4 = findViewById(R.id.btn4);
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String[] urls = {"http://www.ceasewebsite.com/new%20activity12.html", "http://www.ceasewebsite.com/congratsafterlesson4.html", "4"};
+                Intent i = new Intent(Lesson4.this, Module2Web.class);
+                i.putExtra("key", urls);
+                startActivity(i);
+            }
+        });
+
     }
 }
