1531394a57fdb5c2980ac2a000c0a3940996f434
deandraharvin <deandraharvin3@gmail.com>
2021-02-11 15:47:02 -0500

    updating lesson 5

--- app/src/main/java/com/example/android/Lesson5.java
+++ app/src/main/java/com/example/android/Lesson5.java
@@ -2,13 +2,29 @@ package com.example.android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Lesson5 extends AppCompatActivity {
 
+    ImageView btn5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lesson5);
+
+        btn5 = findViewById(R.id.btn5);
+
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String[] urls = {"http://www.ceasewebsite.com/new%20activity15.html", "http://www.ceasewebsite.com/congratsafterlesson5.html", "4"};
+                Intent i = new Intent(Lesson5.this, Module2Web.class);
+                i.putExtra("key", urls);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/android/Module2Web.java
+++ app/src/main/java/com/example/android/Module2Web.java
@@ -2,13 +2,55 @@ package com.example.android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.ImageView;
 
 public class Module2Web extends AppCompatActivity {
+
+    WebView wvMod2;
+    ImageView ivLesson4;
+    String[] results;
+    Web view2 = new Web();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_module2_web);
+
+        ivLesson4 = findViewById(R.id.ivLesson4);
+        wvMod2 = findViewById(R.id.wvMod2);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            results = extras.getStringArray("key");
+            view2.setStartWeb(results[0]);
+            view2.setEndWeb(results[1]);
+        }
+        if (wvMod2.getUrl().equals(view2.getEndWeb())) {
+            ivLesson4.setVisibility(View.VISIBLE);
+        }
+
+        ivLesson4.setOnClickListener(new View.OnClickListener() {
+            String lesson;
+            @Override
+            public void onClick(View v) {
+                if (results[2].equals("4")) {
+                    lesson = "lesson4";
+                } else if(results[2].equals("5")) {
+                    lesson = "lesson5";
+                }else if(results[2].equals("")) {
+                    lesson = "lesson6";
+                }else {
+                    lesson = "end2";
+                }
+
+                Intent i = new Intent(Module2Web.this, Module2Complete.class);
+                i.putExtra("key",lesson);
+                startActivity(i);
+            }
+        });
     }
 }
