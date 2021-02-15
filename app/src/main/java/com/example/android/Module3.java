94d1e0baffa08de3204e40e7c59025a8414505e6
deandraharvin <deandraharvin3@gmail.com>
2021-02-15 13:32:21 -0500

    Making updates to module 3 intro page and adding lesson 7

--- app/src/main/java/com/example/android/Module3.java
+++ app/src/main/java/com/example/android/Module3.java
@@ -2,13 +2,26 @@ package com.example.android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Module3 extends AppCompatActivity {
 
+    ImageView btnMod3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_module3);
+
+        btnMod3 = findViewById(R.id.mod3btn);
+        btnMod3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Module3.this, Lesson7.class);
+                startActivity(i);
+            }
+        });
     }
 }
